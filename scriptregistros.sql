1c981138d8832db715876f0a6b8799c5ab5e72e8
Riaven <jogubaez@hotmail.com>
2020-10-01 23:07:45 -0300

    Crud servicios extra

---
+++ scriptregistros.sql
@@ -0,0 +1,11 @@
+-- MODELO
+-- INSERT INTO TABLA VALUES (valores);
+
+-----------------------------------------
+-- Registros servicios extra
+-----------------------------------------
+
+INSERT INTO SERVICIOS_EXTRA VALUES (155,'Transporte');
+INSERT INTO SERVICIOS_EXTRA VALUES (156,'Internet');
+INSERT INTO SERVICIOS_EXTRA VALUES (157,'Cable');
+INSERT INTO SERVICIOS_EXTRA VALUES (158,'Piscina');
