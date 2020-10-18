f950e34ba7a3db1b7ee5b3ca2db7c2713b0623ef
Riaven <jogubaez@hotmail.com>
2020-10-18 00:26:46 -0300

    crud departamentos con nueva bs

--- crud_departamentos.sql
+++ crud_departamentos.sql
@@ -2,35 +2,34 @@
 -- Procedimiento Crear Departamentos
 ---------------------------------------------
 CREATE OR REPLACE PROCEDURE SP_CREAR_DEPARTAMENTO 
-(P_ID IN NUMBER,
-P_DIRECCION IN VARCHAR2,
-P_NUMERO IN NUMBER,
+(P_ID_DEPA IN NUMBER,
+P_UBICACION IN VARCHAR2,
+P_NUMERO_DEPA IN NUMBER,
 P_PRECIO IN NUMBER,
 P_M_CUADRADOS IN VARCHAR2,
 P_BANIOS IN NUMBER,
-P_DESCRIPCION IN VARCHAR2,
-P_CANTIDAD_HABITACION IN NUMBER,
-P_ID_ESTADO IN NUMBER,
-P_ID_REGION IN NUMBER,
+P_DESC_DEPA IN VARCHAR2, -- DESCRIPCION
+P_CANTI_HABI IN NUMBER,
+P_ID_ESTADO_DEPA IN NUMBER,
+P_ID_EMPLEADO IN NUMBER,
 P_ID_COMUNA IN NUMBER,
 P_ID_INVENTARIO IN NUMBER,
-P_ID_FUNCIONARIO IN NUMBER,
-P_ID_SERVICIOS_EXTRA IN NUMBER,
 P_AFFECTED  OUT NUMBER) AS
 BEGIN
-  INSERT INTO DEPARTAMENTO VALUES (P_ID,P_DIRECCION,
-                                        P_NUMERO,
-                                        P_PRECIO,
-                                        P_M_CUADRADOS,
-                                        P_BANIOS,
-                                        P_DESCRIPCION,
-                                        P_CANTIDAD_HABITACION,
-                                        P_ID_ESTADO,
-                                        P_ID_REGION,
-                                        P_ID_COMUNA,
-                                        P_ID_INVENTARIO,
-                                        P_ID_FUNCIONARIO,
-                                        P_ID_SERVICIOS_EXTRA);
+  INSERT INTO DEPARTAMENTO VALUES (P_ID_DEPA ,
+                                   P_UBICACION ,
+                                   P_NUMERO_DEPA ,
+                                   P_PRECIO ,
+                                   P_M_CUADRADOS ,
+                                   P_BANIOS ,
+                                   P_DESC_DEPA ,
+                                   P_CANTI_HABI ,
+                                   1,
+                                   P_ID_ESTADO_DEPA ,
+                                   P_ID_EMPLEADO ,
+                                   P_ID_COMUNA ,
+                                   P_ID_INVENTARIO 
+                                   );
     P_AFFECTED := SQL%ROWCOUNT;
     COMMIT; -- PARA GUARDAR CAMBIOS
 EXCEPTION
@@ -43,39 +42,35 @@ END SP_CREAR_DEPARTAMENTO;
 -- Procedimiento Modificar Departamentos
 ---------------------------------------------
 CREATE OR REPLACE PROCEDURE SP_MODIFICAR_DEPARTAMENTO 
-(P_ID IN NUMBER,
-P_DIRECCION IN VARCHAR2,
-P_NUMERO IN NUMBER,
+(P_ID_DEPA IN NUMBER,
+P_UBICACION IN VARCHAR2,
+P_NUMERO_DEPA IN NUMBER,
 P_PRECIO IN NUMBER,
 P_M_CUADRADOS IN VARCHAR2,
 P_BANIOS IN NUMBER,
-P_DESCRIPCION IN VARCHAR2,
-P_CANTIDAD_HABITACION IN NUMBER,
-P_ID_ESTADO IN NUMBER,
-P_ID_REGION IN NUMBER,
+P_DESC_DEPA IN VARCHAR2, -- DESCRIPCION
+P_CANTI_HABI IN NUMBER,
+P_ID_ESTADO_DEPA IN NUMBER,
+P_ID_EMPLEADO IN NUMBER,
 P_ID_COMUNA IN NUMBER,
 P_ID_INVENTARIO IN NUMBER,
-P_ID_FUNCIONARIO IN NUMBER,
-P_ID_SERVICIOS_EXTRA IN NUMBER,
 P_AFFECTED OUT NUMBER) AS
 BEGIN
 
   UPDATE DEPARTAMENTO
   SET 
-         DIRECCION = P_DIRECCION,
-         NUMERO = P_NUMERO,
+         UBICACION = P_UBICACION,
+         NUMERO_DEPA = P_NUMERO_DEPA,
          PRECIO = P_PRECIO,
          M_CUADRADOS = P_M_CUADRADOS,
          BANIOS = P_BANIOS,
-         DESCRIPCION = P_DESCRIPCION,
-         CANTIDAD_HABITACION = P_CANTIDAD_HABITACION,
-         ID_ESTADO = P_ID_ESTADO,
-         ID_REGION = P_ID_REGION,
-         ID_COMUNA = P_ID_COMUNA,
-         ID_INVENTARIO = P_ID_INVENTARIO,
-         ID_FUNCIONARIO = P_ID_FUNCIONARIO,
-         ID_SERVICIOS_EXTRA = P_ID_SERVICIOS_EXTRA
-  WHERE ID_DEPARTAMENTO = P_ID;
+         DESC_DEPA = P_DESC_DEPA,
+         CANTI_HABI = P_CANTI_HABI,
+         ESTADO_DEPARTAMENTO_ID_ESTADO = P_ID_ESTADO_DEPA,
+         EMPLEADO_ID_EMPLEADO =  P_ID_EMPLEADO,
+         COMUNA_ID_COMUNA =  P_ID_COMUNA,
+         INVENTARIO_ID_INV = P_ID_INVENTARIO
+  WHERE ID_DEPA = P_ID_DEPA;
   P_AFFECTED := SQL%ROWCOUNT;
   COMMIT; -- PARA GUARDAR CAMBIOS
 EXCEPTION
@@ -93,7 +88,7 @@ CREATE OR REPLACE PROCEDURE SP_ELIMINAR_DEPARTAMENTO
   V_ID_SALIDA NUMBER;
 BEGIN
  
-  DELETE FROM DEPARTAMENTO WHERE ID_DEPARTAMENTO = P_ID;
+  DELETE FROM DEPARTAMENTO WHERE ID_DEPA= P_ID;
   P_AFFECTED := SQL%ROWCOUNT;
   COMMIT; -- PARA GUARDAR CAMBIOS
 EXCEPTION
